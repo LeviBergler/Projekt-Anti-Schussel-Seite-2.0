befbeed3ebfbc9e18bfd9e20872a021102c82bc7
LeviBergler <104757167+LeviBergler@users.noreply.github.com>
2022-05-11 13:35:13 +0200

    Rename 2.0

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,6 +1,6 @@
 const value = localStorage.getItem("youtube.com/achd3euf7")
 if (value !== "youtube.com/gef6b3kl5") {
-    window.location.href = "../Login/login.html"
+    window.location.href = "../login/login.html"
 }
 
 function logout() {
