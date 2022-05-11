c6bb7e2dab7f3f2e72eaadba6a46de546391644b
LeviBergler <104757167+LeviBergler@users.noreply.github.com>
2022-05-11 11:16:02 +0200

    Login/ Logout working

--- Home/index.js
+++ Home/index.js
@@ -1,1 +1,9 @@
-console.log("Test")
+const value = localStorage.getItem("youtube.com/achd3euf7")
+if (value !== "youtube.com/gef6b3kl5") {
+    window.location.href = "../login/login.html"
+}
+
+function logout() {
+    localStorage.removeItem("youtube.com/achd3euf7")
+    window.location.reload()
+}
--- Login/login.js
+++ Login/login.js
@@ -1,3 +1,25 @@
+const value = localStorage.getItem("youtube.com/achd3euf7")
+if (value == "youtube.com/gef6b3kl5") {
+    window.location.href = "../home/index.html"
+}
+
 function login() {
-console.log("Hallo")
+const username = document.getElementById("Username").value
+const password = document.getElementById("Password").value
+
+if (username == atob("TGVocmVy") && password == atob("VXNlcl8xNw==")) {
+    localStorage.setItem("youtube.com/achd3euf7", "youtube.com/gef6b3kl5")
+    window.location.href = "../home/index.html"
+} else {
+    document.getElementById("LoginFailed").innerHTML = "Anmeldung fehlgeschlagen"
+    const button = document.getElementById("loginButton")
+    button.innerHTML = "Gesperrt für 15 Sekunden"
+    button.setAttribute("disabled", "disabled")
+    setTimeout(
+        () => {
+            button.removeAttribute("disabled")
+            button.innerHTML = "Login"
+        }, 15000
+    )
+}
 }
